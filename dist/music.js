705c5a5cd767ea271f15505f2fd034bfabdce935
Rigel-225 <1466603089@qq.com>
2019-11-16 23:31:01 +0800

    Site updated: 2019-11-16 23:31:01

--- dist/music.js
+++ dist/music.js
@@ -6,19 +6,19 @@ const ap = new APlayer({
       {
         name: "River Flows in You",
         artist: 'Yiruma',
-        url: 'https://link.hhtjim.com/163/22829932.mp3',
+        url: 'https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBcG9sYzVHU3djUDhpem9fUk9VVVFaRDVQQmlYP2U9enZ0UmRq.flac',
         cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003zD6X70C6Ti6_1.jpg?max_age=2592000',
       },
       {
         name: "Apologize",
         artist: 'Martin Ermen',
-        url: 'https://link.hhtjim.com/163/1784227.mp3',
+        url: 'https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBcG9sYzVHU3djUDhpem5BNldPcDY3MlBqV24yP2U9NElvUHd0.mp3',
         cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003SC3sk2zNagW.jpg?max_age=2592000',
       },
 	{
         name: "canon in d",
         artist: 'Brian Crain',
-        url: 'https://link.hhtjim.com/163/1091088.mp3',
+        url: 'https://onedrive.gimhoy.com/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBcG9sYzVHU3djUDhpenZKcHFtYlNvZlM0OGs5P2U9NjVZYml3.mp3',
         cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000032l6CH3avQDY_1.jpg?max_age=2592000',
   }
     ]
