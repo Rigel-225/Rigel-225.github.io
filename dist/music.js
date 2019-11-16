ac8ec8c31b739ce96fd459856d916e83433420ce
Rigel-225 <1466603089@qq.com>
2019-11-16 22:18:04 +0800

    Site updated: 2019-11-16 22:18:04

--- dist/music.js
+++ dist/music.js
@@ -6,20 +6,20 @@ const ap = new APlayer({
       {
         name: "River Flows in You",
         artist: 'Yiruma',
-        url: 'http://music.163.com/song/media/outer/url?id=22829932.mp3',
-        cover: 'http://p1.music.126.net/8ZRSyI0ZN_4ah8uzsNd1mA==/2324367581169008.jpg?param=130y130',
+        url: 'https://link.hhtjim.com/163/22829932.mp3',
+        cover: 'https://p1.music.126.net/8ZRSyI0ZN_4ah8uzsNd1mA==/2324367581169008.jpg?param=130y130',
       },
       {
         name: "Apologize",
         artist: 'Martin Ermen',
-        url: 'http://music.163.com/song/media/outer/url?id=1784227.mp3',
-        cover: 'http://p1.music.126.net/-_6mcI4VV5IKaiwhUAytbg==/1791104441647901.jpg?param=130y130',
+        url: 'https://link.hhtjim.com/163/1784227.mp3',
+        cover: 'https://p1.music.126.net/-_6mcI4VV5IKaiwhUAytbg==/1791104441647901.jpg?param=130y130',
       },
 	{
         name: "canon in d",
         artist: 'Brian Crain',
-        url: 'http://music.163.com/song/media/outer/url?id=1091088.mp3',
-        cover: 'http://p1.music.126.net/QGb9Vtyw7qHS00uEvPfM6g==/843325418547559.jpg?param=130y130',
+        url: 'https://link.hhtjim.com/163/1091088.mp3',
+        cover: 'https://p1.music.126.net/QGb9Vtyw7qHS00uEvPfM6g==/843325418547559.jpg?param=130y130',
   }
     ]
 });
